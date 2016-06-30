5d5ee6073515ec69f7845ce1b0f09c28557ad86c
bimofan <bimofan@126.com>
2016-06-30 11:08:32 +0800

    init
    init

---
+++ HallyBoJue/HallyBoJue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HallyBoJue
+//
+//  Created by Haikun Zhu on 16/6/30.
+//  Copyright © 2016年 Haikun Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HallyBoJue/HallyBoJue/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HallyBoJue
+//
+//  Created by Haikun Zhu on 16/6/30.
+//  Copyright © 2016年 Haikun Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
